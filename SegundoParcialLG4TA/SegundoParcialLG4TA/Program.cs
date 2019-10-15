0f6a6036ab99ad262a66e4a454fce8618ad52783
WENMY RANSES MATOS <ranses94@gmail.com>
2019-10-14 23:06:57 -0400

    Creacion del proyecto

---
+++ SegundoParcialLG4TA/SegundoParcialLG4TA/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoParcialLG4TA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
