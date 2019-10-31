c51182c55100b025da9c13c0db94c0ad1f7577ae
WENMY RANSES MATOS <ranses94@gmail.com>
2019-10-30 23:15:30 -0400

    prueba

--- SegundoParcialLG4TA/SQLCONSOLEAPP/Menu/Factura.cs
+++ SegundoParcialLG4TA/SQLCONSOLEAPP/Menu/Factura.cs
@@ -1,48 +1,106 @@
 ﻿using SQLCONSOLEAPP.Data;
+using SQLCONSOLEAPP.Maintenance;
 using SQLCONSOLEAPP.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using SegundoParcialLG4TA.Menu;
 
 namespace SQLCONSOLEAPP.Menu
 {
     //dd
     public class Factura
     {
+        
+        OrderData OD = new OrderData();
+        Orders orders = new Orders();
+        ProductsMaintenance PM = new ProductsMaintenance();
+        public void Imprimir ()
+        {
+            OrdenDetailData ODD = new OrdenDetailData();
+            Console.WriteLine("Inserta el ID de la Factura");
+            var buscar = Console.ReadLine();
+            var x = OD.GetById(Convert.ToInt32(buscar));
+            if (x == null)
+            {
+                Console.WriteLine("No existe Ese numero de orden");
+                Console.ReadKey();
+                MenuPrincipal mnu = new MenuPrincipal();
+                mnu.MenuP();
+            }
+            else
+            {
+                try
+                {
+                    File.WriteAllLines("D:lp4.txt", new String[] { x.OrderID + "\t" + x.CustomerID + "\t" + x.EmployeeID + "\t" + x.OrderDate });
+
+                    Console.WriteLine(x.OrderID + "\t" + x.CustomerID + "\t" + x.EmployeeID + "\t" + x.OrderDate);
+
+                }
+                catch (Exception e)
+                {
+
+                    throw e;
+                }
+
+
+                var Detalle = ODD.ObtenerFacturasDetalle().Where(z => z.OrderID == Convert.ToInt32(buscar));
+
+
+                File.AppendAllLines("D:lp4.txt", new String[] { "IDAriculo\t|\tAriculo\t|\tPrecio\t|\tCantidad\t|\tDescuento\t|\tCargo Total\t| " });
+
+
+                foreach (var item in Detalle)
+                {
+                    var producto = PM.GetById(item.ProductID);
+                    File.AppendAllLines("D:lp4.txt", new String[] { item.ProductID + "\t" + producto.ProductName + "\t" + item.Quantity + "\t" + item.UnitPrice + "\t" + item.Discount });
+                    Console.WriteLine(item.ProductID + "\t" + item.Quantity + "\t" + producto.ProductName + "\t" + item.UnitPrice + "\t" + item.Discount);
+                }
+            }
+            
+
+        }
+
+
+
+
         public void factura()
         {
             decimal monto;
             decimal total = 0;
             int cantidad = 0;
             int productos;
-            DateTime fecha;
-           // DateTime fecha2;
-            DateTime fecha3;
+           
             OrderData OD = new OrderData();
             OrdenDetailData ODD = new OrdenDetailData();
-            // OrdenDetail detalle = new OrdenDetail();
-            Console.WriteLine(" Numero de Factura: ");
-            var IDO= Convert.ToInt16(Console.ReadLine());
+            
+            
             Console.WriteLine(" ID Cliente: ");
             var id= Console.ReadLine();
             Console.WriteLine(" ID empleado: ");
             var ide = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("Fecha de la orden ");
-            fecha = Convert.ToDateTime( Console.ReadLine());         
-            Console.WriteLine(" Fecha de la Envio: ");
-            fecha3 = Convert.ToDateTime( Console.ReadLine());
+           
+            try
+            {
+                var todo = OD.ObtenerFacturas();
+            }
+            catch (Exception e)
+            {
 
+                throw e;
+            }
 
+            
+            
             Orders orders = new Orders()
             {
                 CustomerID = id,
                 EmployeeID = ide,
-                OrderDate = fecha,
-                RequiredDate= fecha,
-                ShippedDate=fecha3
-                        
+                OrderDate = DateTime.Today            
+                                            
             };
 
             try
@@ -52,21 +110,49 @@ namespace SQLCONSOLEAPP.Menu
             catch (Exception e)
             {
 
-                throw e;
-            }      
-            
+                Console.WriteLine("El valor introducido esta incorrecto ");
+                Console.WriteLine("Precione una tecla para volver");
+                Console.ReadKey();
+                MenuPrincipal mnu = new MenuPrincipal();
+                mnu.MenuP();
+            }
+
+
+            var ID = OD.GetAuto();
+
+
             Console.WriteLine(" Nombre Cliente: ");
-           // orden.Nombre_cliente = Console.ReadLine();
+            Console.WriteLine(id);
+
             Console.WriteLine("# de productos a registrar: ");
-            productos = Convert.ToInt16(Console.ReadLine());
+            productos = Convert.ToInt16(Console.ReadLine());    
 
             List<Order_Details> Orden_list = new List<Order_Details>();
-            Console.WriteLine("Introducir Articulo, Precio, Cantidad, Descuento : ");
+            
+            
+
             do
             {
+                Console.WriteLine("Introducir  ID Articulo, Cantidad, Descuento : ");
+                try
+                {
+                    var producto = PM.GetById(Convert.ToInt16(Console.ReadLine()));
+                    Orden_list.Add(new Order_Details(ID.OrderID, producto.ProductID, Convert.ToInt16(producto.UnitPrice), Convert.ToInt16(Console.ReadLine()), Convert.ToInt16(Console.ReadLine())));
+                }
+                catch 
+                {
+                    Console.WriteLine("El valor introducido esta incorrecto ");
+
+                    Console.WriteLine("Precione una tecla para volver");
+                    Console.ReadKey();
+                    MenuPrincipal mnu = new MenuPrincipal();
+                    mnu.MenuP();
+
+                }
                
-             
-                Orden_list.Add(new Order_Details(IDO, Convert.ToInt16(Console.ReadLine()), Convert.ToInt16(Console.ReadLine()), Convert.ToInt16(Console.ReadLine()), Convert.ToInt16(Console.ReadLine())));
+
+               
+                //Orden_list.Add(new Order_Details(ID.OrderID, Convert.ToInt16(Console.ReadLine()), Convert.ToInt16(Console.ReadLine()), Convert.ToInt16(Console.ReadLine()), Convert.ToInt16(Console.ReadLine())));
                              
             }
             while (productos > Orden_list.Count);
@@ -76,18 +162,19 @@ namespace SQLCONSOLEAPP.Menu
             Console.WriteLine();
             Console.WriteLine();
             //Numero de la orden
-            Console.WriteLine("000" + IDO);
+            Console.WriteLine(ID.OrderID);
            //nombre del cliente
-           //Console.Write(orden.Nombre_cliente);
+           
 
             Console.WriteLine();
             //Detalle de la orden
 
-            //Order_Details order_De = new Order_Details();
-            Console.WriteLine("\t Ariculo \t| Precio \t| Cantidad \t| Descuento \t| Cargo Total |  ");
+           
+            Console.WriteLine("IDAriculo\t|Ariculo\t|Precio\t|Cantidad\t|Descuento\t|Cargo Total|  ");
             foreach (Order_Details ord in Orden_list)
             {
                 Order_Details order_De = new Order_Details()
+
                 {
                     OrderID = ord.OrderID,
                     ProductID = ord.ProductID,
@@ -95,20 +182,26 @@ namespace SQLCONSOLEAPP.Menu
                     Quantity = ord.Quantity,
                     Discount = ord.Discount
                 };
+
                 try
                 {
                     ODD.Insertar(order_De);
+                    ODD.SAVE(order_De);
                 }
-                catch (Exception e)
+                catch (Exception )
                 {
-
-                    throw e;
+                    Console.WriteLine("El valor introducido esta incorrecto ");
+                    
+                    Console.WriteLine("Precione una tecla para volver");
+                    Console.ReadKey();
+                    MenuPrincipal mnu = new MenuPrincipal();
+                    mnu.MenuP();
                 }
 
+                var name = PM.GetById(ord.ProductID);
 
-
-                Console.WriteLine(ord.Products + "/t" + ord.Quantity + "/t"
-                    + ord.UnitPrice + "/t" + +(monto = (ord.UnitPrice * ord.Quantity)));
+                Console.WriteLine(ord.ProductID + "\t" +name.ProductName + "\t" + ord.Quantity + "\t"
+                    + ord.UnitPrice + "\t" + +(monto = (ord.UnitPrice * ord.Quantity)));
 
                 cantidad = ord.Quantity + cantidad;
                 total = monto + total;
@@ -117,6 +210,9 @@ namespace SQLCONSOLEAPP.Menu
 
             Console.WriteLine();
             Console.WriteLine("Total De articulos:       " + cantidad + "\t Total A pagar:   " + total);
+
+            OD.Save(orders);
+            
 
             Console.ReadKey();
         }
